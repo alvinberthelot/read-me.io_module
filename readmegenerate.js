d8604619ec0300e1b2b2909a952718c385f19235
imadhy <imadhy.gt@gmail.com>
2018-01-18 15:31:12 +0100

    Specifie that help command is included in the module commander

--- readmegenerate.js
+++ readmegenerate.js
@@ -1,5 +1,11 @@
 #!/usr/bin/env node
 
+/**
+ * import module commander which 
+ * contain the option -h by default
+ * to list all the command with
+ * their arguments
+ */
 const program = module.exports = require('commander');
 
 const extensions = require('./extensions');
