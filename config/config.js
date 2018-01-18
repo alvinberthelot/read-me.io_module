b42d6748f8ca3a215fe637a76093ec5b2696ecfd
L4brax <ant.hess2@gmail.com>
2018-01-18 13:27:33 +0100

    Mise en place des premiers services extensions et templates

--- config/config.js
+++ config/config.js
@@ -1,9 +1,10 @@
 
 let config = {
-    api: {
-        url: 'https://XXX.XXX.XX.XX/',
-        key: 'mlgfmsjrgmrj0009989gselrkgh'
-    }
-}
+  api: {
+    url: 'https://XXX.XXX.XX.XX/',
+    key: 'mlgfmsjrgmrj0009989gselrkgh',
+    port: '80'
+  }
+};
 
 module.exports = config;
--- services/backendServices.js
+++ services/backendServices.js
@@ -1,9 +1,37 @@
+const http = require('http');
+const config = require('./../config/config');
+
+let request = {
+  hostname: config.api.url,
+  port: config.api.port,
+  path: '/',
+  agent: false  // create a new agent just for this one request
+};
+
+function getHealth(){
+
+}
+
 function getExtensions(){
-  
+  request.path = '/extensions';
+  http.get(request, (res) => {
+    if(res.err){
+      console.log('res.err');
+    } else {
+      console.log(res);
+    }
+  });
 }
 
 function getTemplates(){
-  
+  request.path = '/templates';
+  http.get(request, (res) => {
+    if(res.err){
+      console.log('res.err');
+    } else {
+      console.log(res);
+    }
+  });
 }
 
 function generate(ext, template){
@@ -13,5 +41,6 @@ function generate(ext, template){
 module.exports = {
   getExtensions : getExtensions,
   getTemplates : getTemplates,
-  generate : generate
+  generate : generate,
+  getHealth : getHealth
 };
