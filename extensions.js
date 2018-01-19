ae0b0afef5ade2a2b381e829a3f22f96cbb2704f
imadhy <imadhy.gt@gmail.com>
2018-01-19 14:17:56 +0100

    Mergin extensions command in one single file index.js

--- extensions.js
+++
@@ -1,9 +0,0 @@
-const program = require('./readmegenerate');
-
-program
-    .command('extensions')
-    .alias('e')
-    .description('Get available extensions')
-
-    .action(result => console.log('list of extensions'));
-
